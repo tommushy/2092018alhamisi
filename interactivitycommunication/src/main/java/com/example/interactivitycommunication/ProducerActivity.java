02d1486628b845db1d26c5f803be46712c2d4a40
tommushy <43405145+tommushy@users.noreply.github.com>
2019-01-03 10:37:14 +0300

    interactivity com: setup of activities

---
+++ interactivitycommunication/src/main/java/com/example/interactivitycommunication/ProducerActivity.java
@@ -0,0 +1,19 @@
+package com.example.interactivitycommunication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ProducerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_producer);
+        
+        findViewById(R.id.btnBack).setOnClickListener(this::onBack);
+    }
+
+    private void onBack(View view) {
+    }
+}
